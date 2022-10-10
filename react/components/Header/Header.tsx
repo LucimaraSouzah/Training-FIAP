815d651a862d07bab80580cd662cd6df74572790
Lucimara <lucimarasouzah@gmail.com>
2022-10-10 08:38:07 -0300

    React

---
+++ react/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div>Logotipo</div>
+      <nav>Navegação Menu</nav>
+      <div>Caixa de Busca</div>
+    </header>
+  );
+}
---
+++ react/pages/login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+import Home from '../pages/index'
+
+export default function LoginPage() {
+  return (
+    <>
+      <h1>Seja bem vindo ao React</h1>
+      <Header />
+      <Home/>
+    </>
+  );
+}
