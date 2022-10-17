90d6bdd1510aa8e3c4fac1099fbbed274e396361
Lucimara <lucimarasouzah@gmail.com>
2022-10-16 21:01:57 -0300

    React

--- next/pages/register.tsx
+++ next/pages/register.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-image: url("/barco.webp");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+`;
 
 export default function registerPage() {
-  return (
-    <div>register</div>
-  )
+  return <Container>register</Container>;
 }
