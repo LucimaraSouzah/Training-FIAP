53ee0b23aaa00750fcea4d19c3845d68e477c59f
Lucimara <lucimarasouzah@gmail.com>
2022-10-25 10:29:28 -0300

    Nest

---
+++ nest-api-avanade/src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('users')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get('login')
+  getLogin(): string {
+    return this.appService.getLogin();
+  }
+  @Get('register')
+  getRegister(): string {
+    return this.appService.getRegister();
+  }
+}
